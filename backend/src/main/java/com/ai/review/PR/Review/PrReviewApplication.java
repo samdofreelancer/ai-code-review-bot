f524cb29e70093ace2d7155926b8a87f3e4449b3
samdortader <111177480+samdortader@users.noreply.github.com>
2025-07-10 19:46:38 +0700

    add backend project

---
+++ backend/src/main/java/com/ai/review/PR/Review/PrReviewApplication.java
@@ -0,0 +1,13 @@
+package com.ai.review.PR.Review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrReviewApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ai/review/PR/Review/PrReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.review.PR.Review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
